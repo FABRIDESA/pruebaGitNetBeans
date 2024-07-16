72117c1493a0177790a31f588abf4573218e3754
FABRIDESA <fabridesa@gmail.com>
2024-07-16 14:46:42 -0300

    First Commit

---
+++ pruebaGitNetBeans/src/main/java/com/mypage/pruebagitnetbeans/PruebaGitNetBeans.java
@@ -0,0 +1,16 @@
+
+package com.mypage.pruebagitnetbeans;
+
+public class PruebaGitNetBeans {
+
+    public static void main(String[] args) {
+        
+        /*
+        PROBANDO GIT 1
+        - voy a crear una clasePrinci en esto que seria la rama master (principal)
+        - Voy a crear un branch1 donde voy a crear una clase1
+        - y branch2 una clase2
+        */
+        
+    }
+}
---
+++ pruebaGitNetBeans/src/main/java/com/mypage/pruebagitnetbeans/logica/ClasePrinci.java
@@ -0,0 +1,35 @@
+
+package com.mypage.pruebagitnetbeans.logica;
+
+public class ClasePrinci {
+    
+    private int id;
+    private String descripcion;
+
+    public ClasePrinci() {
+    }
+
+    public ClasePrinci(int id, String descripcion) {
+        this.id = id;
+        this.descripcion = descripcion;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+    
+}
